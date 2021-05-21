30065829a7dd9de238cd1a1397179ca97287840b
iamshroud <970370257@qq.com>
2021-05-21 16:09:43 +0800

    2.0版本，应该不会大改了，还有一点小细节整整就行

---
+++ doodle_jump2.0/jump2.0/camera.py
@@ -0,0 +1,28 @@
+"""
+    file name: camera.py
+    effect: 构建摄像机类，打造画面随着主角移动而移动
+"""
+
+from settings import *
+
+
+class Camera(object):
+    def __init__(self, camera_func, width, height, speed):
+
+        self.camera_func = camera_func
+        self.state = Rect(0, 0, width, height)
+        self.lerpSpeed = speed
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+
+    def apply_rect(self, rect):
+        return rect.move(self.state.topleft)
+
+    # 更新函数
+    def update(self, target):
+
+        self.lerpSpeed = int(((HALF_HEIGHT - 70) - self.apply(target).y) / 8)
+
+        if self.apply(target).y < HALF_HEIGHT - 70:
+            self.state.y += self.lerpSpeed
